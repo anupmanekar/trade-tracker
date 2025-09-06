6ea22a3360cf48417b238b9ad912872eed7a1a3e
anupmanekar <anup.manekar@nitorinfotech.com>
2025-09-05 22:46:10 -0400

    Modularized api calls in diff file

--- main.py
+++ main.py
@@ -1,52 +1,6 @@
-import requests
-import json
 import time
 import threading
-
-BITGET_COIN_API = "https://api.bitget.com/api/v2/spot/public/coins?coin={coin}"
-BITGET_SYMBOL_API = "https://api.bitget.com/api/v2/spot/public/symbols?symbol={symbol}"
-BITGET_TICKER_API = "https://api.bitget.com/api/v2/spot/market/tickers?symbol={symbol}"
-
-def get_coin_info(coin):
-    url = BITGET_COIN_API.format(coin=coin)
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        print("Hello from trade-tracker!")
-        print(json.dumps(data, indent=4))
-    else:
-        print("Error fetching data from Bitget API")
-
-def get_symbol_info(symbol):
-    url = BITGET_SYMBOL_API.format(symbol=symbol)
-    response = requests.get(url)
-    if response.status_code == 200:         
-        data = response.json()
-        print("Hello from trade-tracker!")
-        print(json.dumps(data, indent=4))
-        return data
-    else:
-        print("Error fetching data from Bitget API")
-
-def get_ticker_info(symbol):
-    url = BITGET_TICKER_API.format(symbol=symbol)
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        #print("Hello from trade-tracker!")
-        #print(json.dumps(data, indent=4))
-        return data
-    else:
-        print("Error fetching data from Bitget API")
-        return None
-
-def get_current_price_for_ticker(symbol) -> float:
-    ticker_info = get_ticker_info(symbol)
-    if ticker_info and 'data' in ticker_info:
-        for item in ticker_info['data']:
-            if item['symbol'] == symbol:
-                return float(item['lastPr'])
-    return None
+from api_clients import get_ticker_info, get_current_price_for_ticker
 
 if __name__ == "__main__":
     all_ticker_data = get_ticker_info("")  # Test call to ensure function works
