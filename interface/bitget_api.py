ec06fc283bcc675f2c72a65f529323cab9559713
anupmanekar <anup.manekar@nitorinfotech.com>
2025-10-03 11:38:04 -0400

    added actual place order calls

--- interface/bitget_api.py
+++ interface/bitget_api.py
@@ -1,40 +1,59 @@
+import time
 import requests
 import json
 import os
+from dotenv import load_dotenv
 from domain.models import PlaceOrderRequest
 from utils.bitget_signature_utils import generate_signature, get_timestamp
+import logging
 
+load_dotenv()
+# Configure logger
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 BITGET_COIN_API = "https://api.bitget.com/api/v2/spot/public/coins?coin={coin}"
 BITGET_SYMBOL_API = "https://api.bitget.com/api/v2/spot/public/symbols?symbol={symbol}"
 BITGET_TICKER_API = "https://api.bitget.com/api/v2/spot/market/tickers?symbol={symbol}"
 BITGET_PLACE_ORDER_API = "https://api.bitget.com/api/v2/spot/trade/place-order"
 BITGET_ORDER_INFO_API = "https://api.bitget.com/api/v2/spot/trade/orderInfo?orderId={order_id}"
+BITGET_GET_ACCOUNT_ASSETS_API = "https://api.bitget.com/api/v2/spot/account/assets"
 
 API_KEY = os.getenv("BITGET_API_KEY")
 API_SECRET = os.getenv("BITGET_API_SECRET")
 API_PASSPHRASE = os.getenv("BITGET_API_PASSPHRASE")
+
+def get_headers(timestamp, signature):
+    return {
+        "Content-Type": "application/json",
+        "ACCESS-KEY": API_KEY,
+        "ACCESS-SIGN": signature,
+        "ACCESS-TIMESTAMP": str(timestamp),
+        "ACCESS-PASSPHRASE": API_PASSPHRASE
+    }
 
 def get_coin_info(coin):
     url = BITGET_COIN_API.format(coin=coin)
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        print("Hello from trade-tracker!")
-        print(json.dumps(data, indent=4))
+        logger.info(json.dumps(data, indent=4))
     else:
-        print("Error fetching data from Bitget API")
+        logger.error("Error fetching data from Bitget API")
 
 def get_symbol_info(symbol):
     url = BITGET_SYMBOL_API.format(symbol=symbol)
     response = requests.get(url)
     if response.status_code == 200:         
         data = response.json()
-        print("Hello from trade-tracker!")
-        print(json.dumps(data, indent=4))
+        logger.info(json.dumps(data))
         return data
     else:
-        print("Error fetching data from Bitget API")
+        logger.error("Error fetching data from Bitget API")
 
 def get_ticker_info(symbol):
     url = BITGET_TICKER_API.format(symbol=symbol)
@@ -45,7 +64,7 @@ def get_ticker_info(symbol):
         #print(json.dumps(data, indent=4))
         return data
     else:
-        print("Error fetching data from Bitget API")
+        logger.error("Error fetching data from Bitget API")
         return None
 
 def get_current_price_for_ticker(symbol) -> float:
@@ -62,52 +81,102 @@ def get_history_candlestick_data(symbol, granularity, endtime, size=100):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        #print(json.dumps(data, indent=4))
         return data
     else:
-        print("Error fetching data from Bitget API with error:", response.text)
+        logger.error("Error fetching data from Bitget API with error: %s", response.text)
         return None
 
 def place_order(order_request: PlaceOrderRequest) -> dict:
-    print("Placing order with the following details:", order_request.model_dump_json())
+    logger.info("Placing order with the following details: %s", order_request.model_dump_json())
     url = BITGET_PLACE_ORDER_API
     timestamp = get_timestamp()
-    signature = generate_signature(API_SECRET, timestamp, "POST", url, body=order_request.model_dump_json())
-    headers = {
-        "Content-Type": "application/json",
-        "ACCESS-KEY": API_KEY,
-        "ACCESS-SIGN": signature,
-        "ACCESS-TIMESTAMP": timestamp,
-        "ACCESS-PASSPHRASE": API_PASSPHRASE
-    }
-    response = requests.post(url, headers=headers, data=order_request.model_dump_json())
+    relativeUrl = "/api/v2/spot/trade/place-order"
+    signature = generate_signature(API_SECRET, timestamp, "POST", relativeUrl, body=order_request.model_dump_json(exclude_none=True))
+    headers = get_headers(timestamp, signature)
+    response = requests.post(url, headers=headers, data=order_request.model_dump_json(exclude_none=True))
     if response.status_code == 200:
         data = response.json()
-        print("Order placed successfully:")
-        print(json.dumps(data, indent=4))
+        logger.info("Order placed successfully: %s", json.dumps(data))
         return data
     else:
-        print("Error placing order:", response.text)
+        logger.error("Error placing order: %s", response.text)
         return {"status": "error", "message": response.text}
 
 def get_order_info(order_id: str) -> dict:
-    print(f"Checking status for order ID: {order_id}")
+    logger.info("Getting Order Info for order ID: %s", order_id)
     url = BITGET_ORDER_INFO_API.format(order_id=order_id)
     timestamp = get_timestamp()
-    signature = generate_signature(API_SECRET, timestamp, "GET", url, params={"orderId": order_id})
-    headers = {
-        "Content-Type": "application/json",
-        "ACCESS-KEY": API_KEY,
-        "ACCESS-SIGN": signature,
-        "ACCESS-TIMESTAMP": timestamp,
-        "ACCESS-PASSPHRASE": API_PASSPHRASE
-    }
+    relativeUrl = f"/api/v2/spot/trade/orderInfo"
+    signature = generate_signature(API_SECRET, timestamp, "GET", relativeUrl, params={"orderId": order_id})
+    headers = get_headers(timestamp, signature)
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        print("Order status fetched successfully:")
-        print(json.dumps(data, indent=4))
+        # logger.info("Order Information fetched successfully: %s", json.dumps(data))
         return data
     else:
-        print("Error fetching order status:", response.text)
+        logger.error("Error fetching order information: %s", response.text)
+        return {"status": "error", "message": response.text}
+
+def get_account_assets(coin: str = None) -> dict:
+    logger.info("Fetching account assets...")
+    url = BITGET_GET_ACCOUNT_ASSETS_API
+    if coin:
+        url += f"?coin={coin}"
+    timestamp = get_timestamp()
+    relativeUrl = "/api/v2/spot/account/assets"
+    signature = generate_signature(API_SECRET, timestamp, "GET", relativeUrl, params={"coin": coin} if coin else None)
+    headers = get_headers(timestamp, signature)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        #logger.info("Account assets fetched successfully: %s", json.dumps(data))
+        assets_dict = {}
+        for asset in data.get('data', []):
+            coin = asset.get('coin')
+            if coin:
+                assets_dict[coin] = {
+                    'available': float(asset['available']),
+                    'frozen': float(asset['frozen']),
+                    'locked': float(asset['locked']),
+                    'limitAvailable': float(asset['limitAvailable']),
+                    'uTime': asset['uTime']
+                }
+        #logger.info("Parsed account assets: %s", json.dumps(assets_dict))
+        return assets_dict
+    else:
+        logger.error("Error fetching account assets: %s", response.text)
+        return {"status": "error", "message": response.text}
+    
+def cancel_order_by_symbol(symbol: str) -> dict:
+    logger.info("Cancelling all open orders for symbol: %s", symbol)
+    url = "https://api.bitget.com/api/v2/spot/trade/cancel-symbol-order"
+    timestamp = get_timestamp()
+    relativeUrl = "/api/v2/spot/trade/cancel-symbol-order"
+    body = {"symbol": symbol}
+    signature = generate_signature(API_SECRET, timestamp, "POST", relativeUrl, body=json.dumps(body))
+    headers = get_headers(timestamp, signature)
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+    if response.status_code == 200:
+        data = response.json()
+        logger.info("All open orders cancelled successfully: %s", json.dumps(data))
+        return data
+    else:
+        logger.error("Error cancelling orders: %s", response.text)
+        return {"status": "error", "message": response.text}
+
+def get_current_plan_orders() -> dict:
+    logger.info("Fetching current plan orders...")
+    url = "https://api.bitget.com/api/v2/spot/trade/history-orders?symbol=JUPUSDT"
+    timestamp = get_timestamp()
+    relativeUrl = "/api/v2/spot/trade/history-orders"
+    signature = generate_signature(API_SECRET, timestamp, "GET", relativeUrl, params={"symbol": "JUPUSDT"})
+    headers = get_headers(timestamp, signature)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        logger.info("Current plan orders fetched successfully: %s", json.dumps(data))
+        return data
+    else:
+        logger.error("Error fetching current plan orders: %s", response.text)
         return {"status": "error", "message": response.text}
--- order_message_processor.py
+++ order_message_processor.py
@@ -1,65 +1,255 @@
 # Read the message from Redis queue and track the buy order and poll the symbol until the price increases by 5% or decreases by 2%
+import logging
+from math import floor
 import redis
 import json
 import time
-from interface.bitget_api import get_ticker_info
+from interface.bitget_api import cancel_order_by_symbol, get_ticker_info, place_order, get_order_info, get_account_assets, get_symbol_info
+from domain.models import PlaceOrderRequest
 import pprint
 import threading
 
+# Configure logger
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+MAX_TRADING_AMOUNT = 2  # Maximum USDT to use for trading
 polling_symbols = []  # List to keep track of symbols being polled
 
-def poll_price(symbol, order_price):
-    polling_interval = 60  # 1 minute
-    max_attempts = 15
+def truncate_float(value, decimals):
+        factor = 10 ** decimals
+        return int(value * factor) / factor
+
+# def poll_price(symbol, order_price):
+#     polling_interval = 60  # 1 minute
+#     max_attempts = 15
+#     attempts = 0
+#     while attempts < max_attempts:
+#         attempts += 1
+#         time.sleep(polling_interval)
+#         ticker_info = get_ticker_info(symbol)
+#         if ticker_info and 'data' in ticker_info and len(ticker_info['data']) > 0:
+#             last_price = float(ticker_info['data'][0]['lastPr'])
+#             print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Current price for {symbol}: {last_price}")
+#             if last_price >= order_price * 1.02:
+#                 print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} 2% Target Reached {symbol}.")
+#                 break
+#             elif last_price <= order_price * 0.98:
+#                 print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} 2% Stop Loss Reached {symbol}.")
+#                 break
+#             else:
+#                 print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Price change for {symbol} is within range.")
+#         else:
+#             print(f"Could not fetch ticker info for {symbol}.")
+#     if (attempts == max_attempts):
+#         profit_loss = ((last_price - order_price) / order_price) * 100 if last_price else 0
+#         print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Max attempts reached for {symbol}. Selling at market price. P/L: {profit_loss:.2f}%")
+
+#     polling_symbols.remove(symbol)
+
+def scalping_strategy(symbol, price, side="buy", order_type="limit"):
+    symbol_info = get_symbol_info(symbol)
+    quantity_precision = int(symbol_info['data'][0]['quantityPrecision']) if symbol_info and 'data' in symbol_info and 'quantityPrecision' in symbol_info['data'][0] else 2  # Default precision
+    price_precision = int(symbol_info['data'][0]['pricePrecision']) if symbol_info and 'data' in symbol_info and 'pricePrecision' in symbol_info['data'][0] else 2  # Default precision
+    latest_price_for_symbol = get_ticker_info(symbol=symbol)
+    order_price = float(latest_price_for_symbol['data'][0]['lastPr']) if latest_price_for_symbol and 'data' in latest_price_for_symbol and len(latest_price_for_symbol['data']) > 0 else price
+    logger.info(f"Placing order for {symbol} at latest price {order_price}")
+    order_quantity = round(MAX_TRADING_AMOUNT/order_price, quantity_precision)
+    sell_order_id = None
+    order_request = PlaceOrderRequest(
+        symbol=symbol,
+        price=str(order_price),
+        size=str(order_quantity),
+        side=side,
+        orderType=order_type,
+        force="gtc",
+        tpslType="normal",
+        #presetTakeProfitPrice=str(round(order_price * 1.02, 5)),
+        #presetStopLossPrice=str(round(order_price * 0.98, 5)),
+    )
+    sell_order_request = PlaceOrderRequest(
+        symbol=symbol,
+        size=str(order_quantity),
+        side="sell",
+        orderType="limit",
+        price=str(round(order_price * 1.01, price_precision)),  # Target 2% profit
+        force="gtc",
+        tpslType="normal",
+    )
+    response = place_order(order_request)
+    order_info = None
+    if response and response.get('code') == '00000':
+        logger.info(f"Order placed successfully for {symbol}.")
+        start_time = time.time()
+        while True:
+            time.sleep(30)  # Poll every 30 seconds
+            order_info = get_order_info(response['data']['orderId'])
+            if order_info and order_info['data'][0]['status'] in ['filled']:
+                logger.info(f"Order status for {symbol} is filled.")
+                logger.info(f"Placing sell order for {symbol} at price {sell_order_request.price}")
+                coin_assets = get_account_assets(symbol[:-4])
+                sell_order_request.size = str(truncate_float(float(coin_assets[symbol[:-4]]['available']), quantity_precision))  # Update sell size to available balance
+                sell_response = place_order(sell_order_request)
+                if sell_response and sell_response.get('code') == '00000':
+                    sell_order_id = sell_response['data']['orderId']
+                    logger.info(f"Sell order placed successfully for {symbol}. Sell Order ID: {sell_order_id}")
+                else:
+                    logger.error(f"Failed to place sell order for {symbol}: {sell_response}")
+                break
+            if time.time() - start_time > 300:  # 5 minutes
+                logger.warning(f"Order for {symbol} not filled in 5 minutes. Cancelling order.")
+                cancel_order_by_symbol(symbol)
+                break
+    else:
+        logger.error(f"Failed to place order for {symbol}: {response}")
+    if order_info is not None and order_info['data'][0]['status'] in ['filled'] and sell_order_id is not None:
+        get_order_info(response['data']['orderId'])
+        #sell_the_placed_order(order_price, symbol)
+        wait_for_sell_and_place_market_order(sell_order_id, order_price, symbol)
+    polling_symbols.remove(symbol)
+
+# def sell_the_placed_order(order_price, symbol):
+#     # Sell the asset when it crosses 2% profit or 1% loss
+#     account_assets = get_account_assets()
+#     coin = symbol[:-4]
+#     if (account_assets[coin]['frozen'] is None) or (float(account_assets[coin]['frozen']) < 0.01) and (float(account_assets[coin]['available']) < 0.01):
+#         logger.warning(f"No {coin} balance available to sell.")
+#         return
+#     available_balance = f"{account_assets[coin]['available']}"
+#     logger.info(f"Available balance for {coin}: {available_balance}")
+#     # Loop for max 15 minutes and check the price every 1 minute
+#     polling_interval = 60  # 1 minute
+#     max_attempts = 15
+#     attempts = 0
+#     sell_at_market = False
+#     limit_sell_price = 0
+#     while attempts < max_attempts:
+#         attempts += 1
+#         ticker_info = get_ticker_info(symbol)
+#         if ticker_info and 'data' in ticker_info and len(ticker_info['data']) > 0:
+#             last_price = float(ticker_info['data'][0]['lastPr'])
+#             logger.info(f"Current price for {symbol}: {last_price}")
+#             if last_price >= order_price * 1.02:
+#                 logger.info(f"2% Target Reached {symbol}. Selling at market price.")
+#                 sell_at_market = True
+#                 break
+#             elif last_price <= order_price * 0.99:
+#                 logger.info(f"1% Stop Loss Reached {symbol}. Selling at market price.")
+#                 sell_at_market = True
+#                 break
+#             else:
+#                 logger.info(f"Price change for {symbol} is within range.")
+#         else:
+#             logger.error(f"Could not fetch ticker info for {symbol}.")
+#         time.sleep(polling_interval)
+
+#     if (attempts == max_attempts):
+#         sell_at_market = True
+#         logger.warning(f"Max attempts reached for {symbol}. Selling at market price.")
+
+#     if sell_at_market:
+#         sell_order_request = PlaceOrderRequest(
+#             symbol=symbol,
+#             size=str(float(available_balance)),  # Sell all available
+#             side="sell",
+#             orderType="market",
+#             force="gtc",
+#             tpslType="normal",
+#         )
+#         sell_response = place_order(sell_order_request)
+#         logger.info(f"Sell order response: {sell_response}")
+
+def wait_for_sell_and_place_market_order(sell_order_id, buy_price, symbol):
+    polling_interval = 60  # 60 seconds
+    max_attempts = 20  # Poll for max 20 minutes
     attempts = 0
-    while attempts < max_attempts:
+    coin = symbol[:-4]
+    sell_at_market = False
+    order_info = None
+    while not sell_at_market and attempts < max_attempts:
         attempts += 1
-        time.sleep(polling_interval)
+        # Check if the sell order is filled
+        if sell_order_id is None:
+            logger.error(f"No sell order ID provided for {symbol}. Exiting.")
+            return
+        order_info = get_order_info(sell_order_id)
+        if order_info and 'data' in order_info and len(order_info['data']) > 0:
+            if order_info['data'][0]['status'] in ['filled']:
+                logger.info(f"Sell order for {symbol} is filled.")
+                return
+            else:
+                logger.info(f"Sell order status for {symbol}: {order_info['data'][0]['status']}")
+        else:
+            logger.error(f"Could not fetch order info for sell order ID {sell_order_id}.")
         ticker_info = get_ticker_info(symbol)
+        # if latest price drops below 1% of buy price, cancel the sell order and place market sell order
         if ticker_info and 'data' in ticker_info and len(ticker_info['data']) > 0:
             last_price = float(ticker_info['data'][0]['lastPr'])
-            print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Current price for {symbol}: {last_price}")
-            if last_price >= order_price * 1.02:
-                print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} 2% Target Reached {symbol}.")
+            logger.info(f"Current price for {symbol}: {last_price}")
+            if last_price <= buy_price * 0.99:
+                logger.info(f"Price dropped below 1% of buy price for {symbol}. Cancelling sell order and placing market sell order.")
+                sell_at_market = True
                 break
-            elif last_price <= order_price * 0.98:
-                print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} 2% Stop Loss Reached {symbol}.")
-                break
-            else:
-                print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Price change for {symbol} is within range.")
-        else:
-            print(f"Could not fetch ticker info for {symbol}.")
-    if (attempts == max_attempts):
-        profit_loss = ((last_price - order_price) / order_price) * 100 if last_price else 0
-        print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Max attempts reached for {symbol}. Selling at market price. P/L: {profit_loss:.2f}%")
+        time.sleep(polling_interval)
+    if (attempts == max_attempts or sell_at_market):
+        logger.warning(f"Max attempts reached for {symbol} or sell at market triggered. Cancelling any open orders.")
+        cancel_order_by_symbol(symbol)
+        logger.info(f"Selling at market price.")
+        #time.sleep(50)  # Wait for 5 seconds before placing sell order
+        #account_assets = get_account_assets(coin)
+        available_balance = order_info['data'][0]['size'] if order_info and 'data' in order_info and len(order_info['data']) > 0 else 0
+        logger.info(f"Available balance for {coin}: {available_balance}")
+        if available_balance is None or float(available_balance) < 0.01:
+            logger.warning(f"No {coin} balance available to sell.")
+            return
+        sell_order_request = PlaceOrderRequest(
+            symbol=symbol,
+            size=str(available_balance),  # Sell all available
+            side="sell",
+            orderType="market",
+            force="gtc",
+            tpslType="normal",
+        )
+        sell_response = place_order(sell_order_request)
+        pprint.pprint(sell_response)
 
-    polling_symbols.remove(symbol)
 
 def main():
     # Connect to Redis (default localhost:6379)
     r = redis.Redis(host='localhost', port=6379, db=0)
     place_order_queue = 'place_order_requests'  # Queue to place order requests
-    print("Listening for messages... (Ctrl+C to stop)")
+    logger.info("Listening for messages... (Ctrl+C to stop)")
     try:
         while True:
             # BLPOP blocks until a message is available
             message = r.blpop(place_order_queue)
             if message:
-                print(f"Received message at {time.strftime('%Y-%m-%d %H:%M:%S')}: {message[1].decode('utf-8')}")
+                logger.info(f"Received message at {time.strftime('%Y-%m-%d %H:%M:%S')}: {message[1].decode('utf-8')}")
                 # Process the message (for example, fetch historical candlestick data)
                 msg_data = json.loads(message[1].decode('utf-8'))
                 symbol = msg_data.get('symbol')
                 order_price = msg_data.get('price', 0)
+                account_assets = get_account_assets()
+                # for testing add USDT balance. Comment this in production
+                #account_assets['usdt']['available'] = 5
                 if not symbol:
-                    print("No symbol found in message.")
+                    logger.warning("No symbol found in message.")
                     continue
-                if symbol not in polling_symbols:
-                    print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} Order price for {symbol}: {order_price}")
+                if (symbol not in polling_symbols) and (account_assets['USDT']['available'] >= MAX_TRADING_AMOUNT):
+                #if (symbol not in polling_symbols):
+                    logger.info(f"Order price for {symbol}: {order_price}")
                     polling_symbols.append(symbol)
-                    polling_thread = threading.Thread(target=poll_price, args=(symbol, order_price))
+                    polling_thread = threading.Thread(target=scalping_strategy, args=(symbol, order_price))
                     polling_thread.start()
+                else:
+                    logger.warning(f"Already polling {symbol} or insufficient USDT balance.")
     except KeyboardInterrupt:
-        print("Stopping message processor.")
+        logger.info("Stopping message processor.")
 
 if __name__ == "__main__":
     main()
